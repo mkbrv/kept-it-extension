af382655eb7d2e960bc6deca3c55880e1daeb60e
Miklos Csere <miki@softwareengineer.ro>
2017-10-03 19:43:22 +0200

    Initial commit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+
+  }
+
+}
+
+/**
+ {
+   "albumId": 1,
+   "id": 1,
+   "title": "accusamus beatae ad facilis cum similique qui sunt",
+   "url": "http://placehold.it/600/92c952",
+   "thumbnailUrl": "http://placehold.it/150/92c952"
+ }
+ */
+class Photo {
+
+  id: number;
+  albumId: number;
+  title: string;
+  url: string;
+  thumbnail: string;
+
+  constructor(private data: Array<any>) {
+    this.albumId = data['albumId'];
+    this.id = data['id'];
+    this.title = data['title'];
+    this.url = data['url'];
+    this.thumbnail = data['thumbnailUrl'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule} from '@angular/material';
+
+
+import {AppComponent} from './app.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import { BackgroundComponent } from '../modules/background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
